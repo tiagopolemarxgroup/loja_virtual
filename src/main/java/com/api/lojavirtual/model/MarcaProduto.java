4dc9c041209543fac6685847f6faef13c8a6f728
tiago <tiago.polemarxgroup@gmail.com>
2024-07-28 01:04:33 -0300

    entidades e configuraçâo jpa

---
+++ src/main/java/com/api/lojavirtual/model/MarcaProduto.java
@@ -0,0 +1,23 @@
+package com.api.lojavirtual.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+    private String nomeDesc;
+
+}
